cbc42fd509e0ec8f6165abded96886609daf79f3
ericknavarro <thenavarro97@gmail.com>
2020-10-14 00:26:40 -0500

    Add cardviews, adapters and some images

--- app/src/main/java/com/ericknavarro/restaurantapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ericknavarro/restaurantapp/ui/home/HomeFragment.kt
@@ -4,28 +4,59 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ericknavarro.restaurantapp.R
+import com.ericknavarro.restaurantapp.adapter.RecyclerAdapter
+import com.ericknavarro.restaurantapp.model.Card
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: RecyclerAdapter
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
+
+        recyclerView = root.findViewById(R.id.recycler_view)
+        adapter = RecyclerAdapter(root.context)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(root.context)
+
+        /*val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
-        })
+        })*/
+
         return root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val card = Card("Shizen Restaurant & Bar", R.drawable.burger)
+        val card2 = Card("Beef 'n' Pepper", R.drawable.hotcakes)
+        val card1 = Card("Soul Kitchen", R.drawable.hotdog)
+        val card3 = Card("Enoteka Warszawsky", R.drawable.pizza)
+
+        adapter.setCards(arrayListOf(card, card2, card1, card3))
+        /*homeViewModel.allNotes.observe(viewLifecycleOwner, Observer { card ->
+            card?.let { adapter.setCards(card) }
+        })*/
+
+
+
     }
 }
--- app/src/main/java/com/ericknavarro/restaurantapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/ericknavarro/restaurantapp/ui/home/HomeViewModel.kt
@@ -3,6 +3,8 @@ package com.ericknavarro.restaurantapp.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ericknavarro.restaurantapp.R
+import com.ericknavarro.restaurantapp.model.Card
 
 class HomeViewModel : ViewModel() {
 
@@ -10,4 +12,5 @@ class HomeViewModel : ViewModel() {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+
 }
